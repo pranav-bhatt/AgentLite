aaba185ae83f8e445b66cf1972bd24cf8c29bce3
pranav <pranav.b@cloudera.com>
2024-04-30 19:52:29 +0530

    added test keys and fixes

--- agentlite/llm/LLMConfig.py
+++ agentlite/llm/LLMConfig.py
@@ -12,6 +12,7 @@ class LLMConfig:
         self.stop = ["\n"]
         self.max_tokens = 256
         self.end_of_prompt = ""
-        self.api_key: str = "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"
-        self.base_url = "https://api.groq.com/openai/v1"
+        self.api_key: str = "gsk"
+        self.base_url = "https://api.groq.com/openai/v1"  # _LJQF1wYf6kcybN0el
         self.__dict__.update(config_dict)
+        # jkdWGdyb3FYlXNLYUdgNr7AVfoI5I7Rovz8
