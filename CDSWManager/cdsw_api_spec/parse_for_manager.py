d14e1a516a64d6cb2a32c800154783fcdf6df14e
pranav <pranav.b@cloudera.com>
2024-05-14 05:08:02 +0530

    updated bucketed json

--- CDSWManager/cdsw_api_spec/parse_for_manager.py
+++ CDSWManager/cdsw_api_spec/parse_for_manager.py
@@ -2,32 +2,19 @@ import json
 import pprint
 
 
-def managerParser():
-    f = open("dereferenced_full_swagger.json")
-    swagger = json.load(f)
-    f.close()
-
-    metadata = {}
-    for path in swagger["paths"]:
-        metadata[path] = {}
-        for HTTPMethod in swagger["paths"][path]:
-            metadata[path][HTTPMethod] = swagger["paths"][path][HTTPMethod]["summary"]
-    json.dump(metadata, open("manager_metadata.json", "w"), indent=4)
-
-
-def remove_useless_keys(dictionary, useless_keys):
+def remove_unecessary_keys(dictionary, useless_keys):
     if isinstance(dictionary, dict):
         for key, value in list(dictionary.items()):
             if key in useless_keys:
                 del dictionary[key]
             else:
-                remove_useless_keys(value, useless_keys)
+                remove_unecessary_keys(value, useless_keys)
     elif isinstance(dictionary, list):
         for item in dictionary:
-            remove_useless_keys(item, useless_keys)
+            remove_unecessary_keys(item, useless_keys)
 
 
-def swaggerSplitter():
+def swaggerSplitter(threshold=2):
     f = open("dereferenced_full_swagger.json")
     swagger = json.load(f)
     f.close()
@@ -41,22 +28,47 @@ def swaggerSplitter():
         "operationId",
         "tags",
     ]
-    remove_useless_keys(swagger, useless_keys)
+    remove_unecessary_keys(swagger, useless_keys)
 
-    sections = {}
-    for k, v in swagger["paths"].items():
-        section_name = k.split("/")[3]
-        if section_name not in sections:
-            sections[section_name] = {k: v}
-        else:
-            sections[section_name][k] = v
+    metadata = {}
+    buckets = {}
+    for path, methods in swagger["paths"].items():
+        path_parts = path.split("/")
+        bucket_name = (
+            "_".join(path_parts[3 : 3 + threshold])
+            if len(path_parts) > 3
+            else path_parts[3]
+        )
+        while bucket_name in buckets and len(buckets[bucket_name]) >= threshold:
+            threshold += 1
+            bucket_name = (
+                "_".join(path_parts[3 : 3 + threshold])
+                if len(path_parts) > 3
+                else path_parts[3]
+            )
 
-    for k, v in sections.items():
+        if bucket_name not in buckets:
+            buckets[bucket_name] = {}
+
+        buckets[bucket_name][path] = methods
+
+    for bucket_name, paths in buckets.items():
+        # set the initial data for manager metadata along with where the files are being stored
+        for path in paths:
+            metadata[path] = {}
+            metadata[path]["methods"] = {}
+            metadata[path]["file"] = f"{bucket_name}.json"
+
+            for method in paths[path]:
+                metadata[path]["methods"][method] = paths[path][method]["summary"]
+
+        # populate the individual json files with api information
         json.dump(
-            v,
-            open(f"agent_categorised_json/{k}.json", "w"),
+            paths,
+            open(f"agent_categorised_json/{bucket_name}.json", "w"),
             separators=(",", ":"),
         )
+    json.dump(metadata, open("manager_metadata.json", "w"), indent=4)
 
 
 if __name__ == "__main__":
